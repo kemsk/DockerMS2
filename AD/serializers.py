bccf48eeb42289189517bfa1067e116cdc738b29
Glenn Michael C. Deguit <20210024060@my.xu.edu.ph>
2025-05-07 00:12:25 +0800

    1054 error

---
+++ AD/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import PhotoUpload
+
+class PhotoUploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhotoUpload
+        fields = ['id', 'title', 'image', 'uploaded_at', 'student']
+
+    def validate_image(self, value):
+        valid_extensions = ['.jpeg', '.jpg']
+        if not any(value.name.lower().endswith(ext) for ext in valid_extensions):
+            raise serializers.ValidationError("Only .jpeg or .jpg files are allowed.")
+        return value
