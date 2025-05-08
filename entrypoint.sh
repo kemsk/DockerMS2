64cb1239c2a21c1d6098521b1bf94af4ceae6aec
Kimberly Flores <20220024573@my.xu.edu.ph>
2025-05-09 00:01:25 +0800

    delete

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-
-# Start the application using Gunicorn
-python -m gunicorn --bind 0.0.0.0:8003 --workers 3 XUSSIO_EVS.wsgi:application
